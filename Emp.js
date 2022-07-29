d35b98cfea24c6856a11f2ee029ac4a7e7788a04
vishangi <vishangid@gmail.com>
2022-07-30 01:20:04 +0530

    Added : Check employee present or absent

---
+++ Emp.js
@@ -0,0 +1,12 @@
+console.log("Welcome to Employee Wage Program");
+//UC1--> Employee is Present or not
+{
+    const IS_PRESENT=1;
+    let empcheck=Math.floor(Math.random()*2);
+    if(empcheck==IS_PRESENT){
+        console.log("Employee is Present");
+    }else{
+        console.log("Employee is Absent");
+    }
+}
+
