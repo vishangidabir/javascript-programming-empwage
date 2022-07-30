219029a1cf4507145d75416811d50537504e6cb9
vishangi <vishangid@gmail.com>
2022-07-30 23:19:21 +0530

    Added : Using function calculating Employee Wage

--- EmployeeWages.js
+++ EmployeeWages.js
@@ -1,26 +1,27 @@
 console.log("Welcome to Employee Wage Program !");
-///UC2-->Calculating Employee Wage
+//UC3-->To Write Function For Daily Working Hours
 {
     const IS_PART_TIME=1;
     const IS_FULL_TIME=2;
     const PART_TIME_HOURS=4;
     const FULL_TIME_HOURS=8;
     const WAGE_PER_HOUR=20;
-    let empHr=0;
-    empCheck=Math.floor(Math.random()*3);
-    switch (empCheck) {
-        case IS_PART_TIME:
-            empHr=PART_TIME_HOURS;            
-            break;
-        case IS_FULL_TIME:
-            empHr=FULL_TIME_HOURS;
-            break
-        default:
-            empHr=0;
-            break;
+    function getWorkingHours(empCheck){
+        switch(empCheck){
+            case IS_PART_TIME:
+                return PART_TIME_HOURS;
+            case IS_FULL_TIME:
+                return FULL_TIME_HOURS;
+            default:
+                return 0;
+        }
     }
+    let empHr=0;
+    let empCheck=Math.floor(Math.random()*3);
+    empHr=getWorkingHours(empCheck);
     let empWage=empHr*WAGE_PER_HOUR;
-    console.log("UC2-->Employee Wage is :"+empWage);
+    console.log("UC3--> Employee Daily wage :"+empWage);
 }
+
 
 
