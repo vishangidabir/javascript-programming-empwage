cf7ae1d5541c21ff83ed4d61d0bac3faf5a26948
vishangi <vishangid@gmail.com>
2022-07-30 23:33:03 +0530

    Added : Update employee wage

--- EmployeeWages.js
+++ EmployeeWages.js
@@ -1,5 +1,5 @@
 console.log("Welcome to Employee Wage Program !");
-//UC5-->Calculating Wages Still condition Reached
+//UC6-->Storing The Daily Wage in a Array
 {
     const MAXIMUM_WORKING_DAYS=20;
     const IS_PART_TIME=1;
@@ -7,7 +7,7 @@ console.log("Welcome to Employee Wage Program !");
     const PART_TIME_HOURS=4;
     const FULL_TIME_HOURS=8;
     const WAGE_PER_HOUR=20;
-    const MAX_HOURS_IN_MONTH=100;
+    const MAX_HOURS_IN_MONTH=160;
     function getWorkingHours(empCheck){
         switch(empCheck){
             case IS_PART_TIME:
@@ -16,18 +16,26 @@ console.log("Welcome to Employee Wage Program !");
                 return FULL_TIME_HOURS;
             default:
                 return 0;
+            }
         }
+    function calDailyWage(emphr){
+        return emphr * WAGE_PER_HOUR;
     }
     let totalEmpHr=0;
     let totalWorkingDays=0;
+    let emolpoyeeDailyWageArr = new Array();
     while(totalEmpHr<=MAX_HOURS_IN_MONTH && totalWorkingDays<MAXIMUM_WORKING_DAYS){
         totalWorkingDays++;
         let empCheck=Math.floor(Math.random()*3);
-        totalEmpHr+=getWorkingHours(empCheck);
-    }
-    let empWage=totalEmpHr * WAGE_PER_HOUR;
-    console.log("UC5--> Total Days :"+totalWorkingDays+" Total Hours :"+totalEmpHr+" EmployeeWage is : "+empWage);
+        let empHr = getWorkingHours(empCheck)
+        totalEmpHr+=empHr;
+        emolpoyeeDailyWageArr.push(calDailyWage(empHr));   
 }
+let empWage = calDailyWage(totalEmpHr);
+console.log("UC6--> Total Days :"+ totalWorkingDays+ " Total Hours :"+totalEmpHr+" Employee Wage "+empWage);
+}
+
+
 
 
 
